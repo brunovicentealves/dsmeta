ecfc9e4d881e8fb7ca9b1eacb591e940989b10df
Z253534 <z253534@corp.clarobr>
2022-07-11 15:58:23 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
+import NotificationButton from "./assets/components/NotificationButton"
+
+
 
 function App() {
   return (
-  <h1>olá mundo! </h1>)
+    <>
+    <h1>olá mundo! </h1>
+    <NotificationButton/>
+    </>
+  
+  )
+  
 }
 
 export default App
