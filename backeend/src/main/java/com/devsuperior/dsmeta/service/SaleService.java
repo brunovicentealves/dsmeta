00438cbda1a67695b060ca96376d60df5329f3e7
Z253534 <z253534@corp.clarobr>
2022-07-14 18:41:05 -0300

    Twilio sms

--- backeend/src/main/java/com/devsuperior/dsmeta/service/SaleService.java
+++ backeend/src/main/java/com/devsuperior/dsmeta/service/SaleService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dsmeta.service;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,22 +13,27 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repository.SaleRepository;
 
-
 @Service
 public class SaleService {
-	
-	
+
 	@Autowired
 	private SaleRepository repository;
-	
-	public Page<Sale> findSales(String minDate , String maxDate ,Pageable pageable) {
-		
+
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
 		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
-		
-		LocalDate min = minDate.equals("")? today.minusDays(365) : LocalDate.parse(minDate);
-		LocalDate max = maxDate.equals("")? today : LocalDate.parse(maxDate);
-		
+
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
 		return repository.findAll(min, max, pageable);
+	}
+
+	public Sale getSaleId(Long saleId) {
+
+		Optional<Sale> sale = Optional.of(repository.findById(saleId).orElseThrow(RuntimeException::new));
+
+		return sale.get();
 	}
 
 }
